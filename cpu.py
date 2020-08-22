f92c0ccd2591b27eb04ff010e47bb684a845270e
Zernach <ryan@zernach.com>
2020-08-22 11:00:52 -0400

    removes un-used code, converts to constants to binary

--- cpu.py
+++ cpu.py
@@ -6,13 +6,13 @@
 
 import sys
 
-cmp = 0b10100111
-jmp = 0b01010100
-jeq = 0b01010101
-jne = 0b01010110
-hlt = 0b00000001
-ldi = 0b10000010
-prn = 0b01000111
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
 
 class CPU:
     """Main CPU class."""
@@ -22,14 +22,12 @@ class CPU:
         self.ram = [0] * 256  # the amount of working memory in the hardware
         self.reg = [0] * 8
         self.pc = 0  # program counter - which instruction, or which line is curently being read
-        self.fl = 0  # flag - integer, always eight bits
+        self.fl = 0b00000000  # flag - integer, always eight bits
         self.running = True
         # bitwise - binary conversion equivalent
 
-    def load(self, program):
+    def load(self, program=''):
         """Load a program into memory."""
-
-        address = 0
 
         self.ram = [0b10000010,
         0b00000000,
@@ -111,66 +109,78 @@ class CPU:
         # TEST5 (address 73):
         0b00000001] # HLT
 
-       # with open(program) as file:
-       #     for instruction in file:
-       #         if instruction[0] == '0' or instruction[0] == '1':
-       #             self.ram[address] = int(instruction[0:7], 2)
-       #             address += 1
+        # Not being used to read file at the moment:
+
+        #address = 0
+        #with open(program) as file:
+        #    for line in file:
+        #        split_line = line.split('#')[0]
+        #        command = split_line.strip()
+        #        if command == '':
+        #            continue
+        #        instruction = int(command, 2)
+        #        self.ram[address] = instruction
+        #        address += 1
 
     def alu(self, op, reg_a = 0, reg_b = 0):
         """ALU operations."""
 
+        def iterate():
+            self.pc += 1
+
         if op == 'ADD':
             self.reg[reg_a] += self.reg[reg_b]
 
-        # # # SPRINT CHALLENGE MVP # # # 
-        elif op == 'CMP':
+        if op == 'CMP':
             value_a = self.reg[reg_a]
             value_b = self.reg[reg_b]
             if value_a == value_b:
-                self.reg[self.fl] = 1
+                self.reg[self.fl] = 0b00000001 # 1
             elif value_a > value_b:
-                self.reg[self.fl] = 2
+                self.reg[self.fl] = 0b00000010 # 2
             else:
-                self.reg[self.fl] = 4
+                self.reg[self.fl] = 0b00000100 # 4
+            iterate()
 
-        elif op == 'JMP':
+        if op == 'JMP':
             self.pc = self.reg[reg_a]
+            iterate()
             return True
 
-        elif op == 'JNE':
+        if op == 'JNE':
             value = self.reg[self.fl]
-            if value == 2 or value == 4:
+            if value == 0b00000010 or value == 0b00000100:
+                iterate()
                 return self.alu('JMP', reg_a)
 
-        elif op == 'JEQ':
-            if self.reg[self.fl] == 1:
+        if op == 'JEQ':
+            if self.reg[self.fl] == 0b00000001:
+                iterate()
                 return self.alu('JMP', reg_a)
-
-        # # # SPRINT CHALLENGE MVP # # # 
 
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+# Currently Un-Used Function:
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
-        print()
+#    def trace(self):
+#        """
+#        Handy function to print out the CPU state. You might want to call this
+#        from run() if you need help debugging.
+#        """
+#
+#        print(f"TRACE: %02X | %02X %02X %02X |" % (
+#            self.pc,
+#            #self.fl,
+#            #self.ie,
+#            self.ram_read(self.pc),
+#            self.ram_read(self.pc + 1),
+#            self.ram_read(self.pc + 2)
+#        ), end='')
+#
+#        for i in range(8):
+#            print(" %02X" % self.reg[i], end='')
+#        print()
 
     def run(self):
         """Run the CPU."""
@@ -180,35 +190,35 @@ class CPU:
             print(instruction)
             print(self.pc)
 
-            if instruction == cmp:
+            if instruction == CMP:
                 self.alu("CMP", reg_a=self.ram[self.pc+1], reg_b=self.ram[self.pc+2])
-                self.pc += 3
+                #self.pc += 3
 
-            if instruction == jmp:
+            if instruction == JMP:
                 self.alu("JMP", reg_a=self.ram[self.pc+1])
-                self.pc += 2
+                #self.pc += 2
 
-            if instruction == jeq:
+            if instruction == JEQ:
                 self.alu("JEQ", reg_a=self.ram[self.pc+1])
-                self.pc += 2
+                #self.pc += 2
 
-            if instruction == jne:
+            if instruction == JNE:
                 self.alu("JNE", reg_a=self.ram[self.pc+1])
-                self.pc += 2
+                #self.pc += 2
 
-            if instruction == hlt:
+            if instruction == HLT:
                 self.running = False
 
-            if instruction == ldi:
+            if instruction == LDI:
                 # `LDI register immediate`
                 # Set the value of a register to an integer.
                 # Machine code:
                 # 10000010 00000rrr iiiiiiii
                 # 82 0r ii
-                self.reg[self.ram[self.pc+1]] = self.pc + 2
-                self.pc += 3
+                self.reg[self.ram[self.pc+1]] = self.ram[self.pc + 2]
+                #self.pc += 3
 
-            if instruction == prn:
+            if instruction == PRN:
                 # `PRN register` pseudo-instruction
                 # Print numeric value stored in the given register.
                 # Print to the console the decimal integer value that is stored in the given register.
@@ -216,7 +226,7 @@ class CPU:
                 # 01000111 00000rrr
                 # 47 0r````
                 print(self.reg[self.ram[self.pc+1]])
-                self.pc += 2
+                #self.pc += 2
     
     def ram_read(self, address):
         return self.ram[address]
